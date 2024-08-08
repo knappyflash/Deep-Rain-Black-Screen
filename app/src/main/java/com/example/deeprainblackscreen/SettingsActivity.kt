d87e8ba450a8f076d48d6100ef5a79c54d6c65c8
knappyflash <knappyflash2@gmail.com>
2024-08-07 19:53:48 -0700

    Sleep well

--- app/src/main/java/com/example/deeprainblackscreen/SettingsActivity.kt
+++ app/src/main/java/com/example/deeprainblackscreen/SettingsActivity.kt
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import androidx.activity.OnBackPressedCallback
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.preference.SwitchPreferenceCompat
+import android.graphics.Color
+import android.widget.TextView
+import androidx.preference.PreferenceViewHolder
 
 class SettingsActivity : AppCompatActivity() {
 
+    private lateinit var mySwitchPreferenceCompat: SwitchPreferenceCompat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
+//        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
 
         // Handle the back button press
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
@@ -42,6 +50,11 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val showClockPreference = findPreference<SwitchPreferenceCompat>("show_clock")
+
+
+
         }
     }
 }
